1c2e7e78b202ddb60cb298d3e927c94e0b718aed
Santhosh C Y <49633979+santhoshcy@users.noreply.github.com>
2019-06-18 21:42:34 +0530

    Create push.js

---
+++ push.js
@@ -0,0 +1,10 @@
+function inLine(arr,item)
+{
+    arr.push(item);                     // adding element to last index pf arrray
+
+}
+var arr=[1,2,3,4,5];
+
+console.log(arr);
+inLine(arr,6);
+console.log(arr);
