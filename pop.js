e9a9f9f8ea2c37abcefbffdd3241e9e4a73cc166
Santhosh C Y <49633979+santhoshcy@users.noreply.github.com>
2019-06-18 21:46:13 +0530

    Update pop.js

--- pop.js
+++ pop.js
@@ -10,4 +10,17 @@ inLine(arr,5);
 console.log(arr);
 
 
+/* function inLine()
+{
+    arr.pop();      //pops last element present in an array
+
+}
+var arr=[1,2,3,4,5];
+
+console.log(arr);
+inLine();
+console.log(arr);
+
+*/
+
 
